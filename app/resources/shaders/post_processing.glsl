0f8af552adec7eeffee0c024adec2358d8438019
Antonije Micic <micicantonije154@gmail.com>
2026-09-12 04:26:16 +0200

    Add post-processing effects controls

--- app/resources/shaders/post_processing.glsl
+++ app/resources/shaders/post_processing.glsl
@@ -1,5 +1,54 @@
-#version 100
+//#shader vertex
+#version 330 core
+
+layout (location = 0) in vec2 aPos;
+layout (location = 1) in vec2 aTexCoords;
+
+out vec2 TexCoords;
 
 void main() {
+    TexCoords = aTexCoords;
 
+    gl_Position =
+    vec4(aPos.x, aPos.y, 0.0, 1.0);
+}
+
+//#shader fragment
+#version 330 core
+
+out vec4 FragColor;
+
+in vec2 TexCoords;
+
+uniform sampler2D screen_texture;
+uniform int effect;
+
+void main() {
+    vec4 color =
+    texture(screen_texture, TexCoords);
+
+    if (effect == 1) {
+        FragColor =
+        vec4(
+        vec3(1.0) - color.rgb,
+        1.0);
+
+        return;
+    }
+
+    if (effect == 2) {
+        float gray =
+        0.2126 * color.r
+        + 0.7152 * color.g
+        + 0.0722 * color.b;
+
+        FragColor =
+        vec4(
+        vec3(gray),
+        1.0);
+
+        return;
+    }
+
+    FragColor = color;
 }
--- app/src/MainApp.cpp
+++ app/src/MainApp.cpp
@@ -2,14 +2,30 @@
 #include <app/MainApp.hpp>
 #include <app/MainController.hpp>
 
+#include <engine/graphics/PostProcessingController.hpp>
+
 namespace app {
     void MainApp::app_setup() {
-        auto main_controller = register_controller<MainController>();
-        auto gui_controller  = register_controller<GUIController>();
+        auto main_controller =
+                register_controller<MainController>();
+
+        auto post_processing_controller =
+                register_controller<
+                    engine::graphics::
+                    PostProcessingController>();
+
+        auto gui_controller =
+                register_controller<GUIController>();
 
         main_controller->after(
-            engine::core::Controller::get<engine::core::EngineControllersEnd>());
+            engine::core::Controller::get<
+                engine::core::
+                EngineControllersEnd>());
 
-        gui_controller->after(main_controller);
+        post_processing_controller->after(
+            main_controller);
+
+        gui_controller->after(
+            post_processing_controller);
     }
 } // namespace app
